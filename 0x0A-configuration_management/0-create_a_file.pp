62b54169b17711a34e54acd0caf1bb75e6f8f91d
Mano224 <mano@example.com>
2024-09-07 04:30:57 +0300

    hh

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,10 +2,10 @@
 # The file (School) has permissions 0744, belonging to the user and group
 # www-data.
 file {
-	'/tmp/school':
-	ensure => present,
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet',
+        '/tmp/school':
+        ensure => present,
+        mode => '0744',
+        owner => 'www-data',
+        group => 'www-data',
+        content => 'I love Puppet',
 }
