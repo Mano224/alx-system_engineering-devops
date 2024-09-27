ecaf17ecd1fc15951809dcc04414f25c4cb1ce2c
Mano224 <mano@example.com>
2024-09-28 01:21:39 +0300

    d

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,35 +1,40 @@
-import requests
-import sys
-
-def get_employee_todo_progress(employee_id):
-    user_url_api = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todo_url_api = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-
-    user_response = requests.get(user_url_api)
-    if user_response.status_code != 200:
-        print("erorr")
-        return
-    user_data =  user_response.json()
-
-    todo_response = requests.get(todo_url_api)
-    if todo_response.status_code != 200:
-        print("erorr")
-        return
-    todo_data = todo_response.json()
-
-    #processing data
-    employee_name = user_data['name']
-    total_tasks = len(todo_data)
-    completed_tasks = [task for task in todo_data if task['completed']]
-    num_of_completed = len(completed_tasks)
-
-    print(f"Employee {employee_name} id done with tasks({num_of_completed}\{total_tasks}):")
-    for tasks in completed_tasks:
-        print(f"\t {tasks['title']}")
+#!/usr/bin/python3
+""" Gather data from an API  """
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script_name.py <employee_id>")
-    else:
-        employee_id = sys.argv[1]
-        get_employee_todo_progress(employee_id)
+    from requests import get
+    from sys import argv, exit
+
+    try:
+        id = argv[1]
+        is_int = int(id)
+    except:
+        exit()
+
+    url_user = "https://jsonplaceholder.typicode.com/users?id=" + id
+    url_todo = "https://jsonplaceholder.typicode.com/todos?userId=" + id
+
+    r_user = get(url_user)
+    r_todo = get(url_todo)
+
+    try:
+        js_user = r_user.json()
+        js_todo = r_todo.json()
+
+    except ValueError:
+        print("Not a valid JSON")
+
+    if js_user and js_todo:
+        EMPLOYEE_NAME = js_user[0].get('name')
+        TOTAL_NUMBER_OF_TASKS = len(js_todo)
+        NUMBER_OF_DONE_TASKS = sum(item.get("completed")
+                                   for item in js_todo if item)
+
+        print("Employee {} is done with tasks({}/{}):"
+              .format(EMPLOYEE_NAME,
+                      NUMBER_OF_DONE_TASKS,
+                      TOTAL_NUMBER_OF_TASKS))
+        for todo in js_todo:
+            TASK_TITLE = todo.get('title')
+            if todo.get("completed"):
+                print("\t {}".format(TASK_TITLE))
