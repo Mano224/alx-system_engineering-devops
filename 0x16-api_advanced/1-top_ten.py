6ed8d6c004172383e47106f511122515cd516e14
Mano224 <mano@example.com>
2024-10-12 07:01:40 +0300

    s

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""prints the titles of the first 10 hot posts listed for a given subreddit."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """prints the titles of the first 10 hot posts listed for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {"limit": 10}
+    response = requests.get(url, headers=headers,params=params ,allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("title")
