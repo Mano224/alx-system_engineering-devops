139759ca053df919bd622848cfa0f9e031451e73
Mano224 <mano@example.com>
2024-10-12 06:49:41 +0300

    c

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''returns the number of subscribers for a given subreddit'''
+import requests
+
+def number_of_subscribers(subreddit):
+    """ returns the total of subctibers for a given subreddit"""
+    subred_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(subred_url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    data = response.json().get('data')
+    return data.get('subscribers')
